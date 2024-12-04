e5474f863c27fa58e1975b1de6bf87cf092b8641
SIVA-MANOJ <sivamanoj35@gmail.com>
2024-12-04 20:32:33 +0530

    file : first commit

---
+++ Diamond_pattern.py
@@ -0,0 +1,9 @@
+
+n=11        # number of stars in a row
+for i in range(n):      # i --> row
+    for j in range(n):      # j --> column
+        if (i+j>=n//2 and j-i<=n//2)and(i-j<=n//2 and i+j<=n+((n//2)-1)):
+            print("*",end=' ')
+        else:
+            print(" ",end=' ')
+    print()
