b47ccb55bf718784b4bc62fda8f4d9465d277f9f
SIVA-MANOJ <sivamanoj35@gmail.com>
2024-12-21 19:22:13 +0530

    file: program-4 added

--- minimum_currency_notes.py
+++ minimum_currency_notes.py
@@ -1,3 +1,9 @@
+"""
+Question Link:
+https://www.geeksforgeeks.org/find-number-currency-notes-sum-upto-given-amount/
+
+"""
+
 currency_notes=[2000, 500, 200, 100, 50, 20, 10, 5, 1]
 inp=2456
 ans={2000: 0, 500: 0, 200: 0, 100: 0, 50: 0, 20: 0, 10: 0, 5: 0, 1: 0}
