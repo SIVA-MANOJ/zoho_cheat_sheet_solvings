2c36f66513c440782a5acb4f9f7331b31abdfe2f
SIVA-MANOJ <sivamanoj35@gmail.com>
2024-12-23 18:54:18 +0530

    file: number conversion files added

---
+++ binary_to_hex.py
@@ -0,0 +1,5 @@
+
+# The hex() method in Python converts any number system into its hexadecimal #representation. 
+# It returns a string that starts with "0x" followed by the #hexadecimal digits.
+
+print(hex(0b1110100101))    #0x3A5
---
+++ hex_to_binary.py
@@ -0,0 +1,5 @@
+
+# The bin() method in Python converts any number system into its binary #representation. 
+# It returns a string that starts with "0b" followed by the #binary digits.
+
+print(bin(0x3A5))   # 0b1110100101
