3cfbe12033337260979df73bc1936dc28dc74eda
SIVA-MANOJ <sivamanoj35@gmail.com>
2025-01-04 19:31:09 +0530

    file: program-14 added

---
+++ Index_of_an_Extra_Element.py
@@ -0,0 +1,7 @@
+"""
+You have given two sorted arrays arr1[] & arr2[] of distinct elements.
+The first array has one element extra added in between. Return the index of the extra element.
+"""
+inp1 = [2,4,6,8,9,10,12]
+inp2 = [2,4,6,8,10,12]
+print(inp1.index((set(inp1).difference(set(inp2)).pop())))
